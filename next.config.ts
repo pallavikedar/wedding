ddde6ced9a282c6956410a846ee648193226a9eb
Pallavi <kedarpallavi723@gmail.com>
2026-05-02 21:32:14 +0530

    changes done now

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  experimental: {
-    reactCompiler: false,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
+
 export default nextConfig;
