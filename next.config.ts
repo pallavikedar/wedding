0e071f24fee825ca2678c40e83cb666bc05bd35d
Pallavi <kedarpallavi723@gmail.com>
2026-05-02 20:44:49 +0530

    Fix eslint config and remove fix-chart.js

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
-  images: {
-    unoptimized: true,
+  experimental: {
+    reactCompiler: false,
   },
-}
-
-export default nextConfig
+};
+export default nextConfig;
